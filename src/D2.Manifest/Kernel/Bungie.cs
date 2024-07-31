8c90c6c4ca73f1b5feec6c585ae54322f783e261
Joseph <tanglesites@hotmail.com>
2024-07-31 01:06:19 -0500

    Add Infrastructure

---
+++ src/D2.Manifest/Kernel/Bungie.cs
@@ -0,0 +1,8 @@
+﻿namespace D2.Manifest.Kernel;
+
+public class Bungie
+{
+    public const string Section = "Bungie";
+    public string BaseAddress { get; set; } = null!;
+    public string ApiKey { get; set; } = null!;
+}
---
+++ src/D2.Manifest/Kernel/ConnectionStrings.cs
@@ -0,0 +1,8 @@
+﻿namespace D2.Manifest.Kernel;
+
+public class ConnectionStrings
+{
+    public const string Section = "ConnectionStrings";
+    public string D2DataLake { get; set; } = null!;
+    public string D2DataWarehouse { get; set; } = null!;
+}
