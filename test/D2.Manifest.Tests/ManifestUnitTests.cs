ecd8e8fa2081e789faf62c56258f6fc10c3033bf
Joseph <tanglesites@hotmail.com>
2024-07-31 02:44:38 -0500

    Add Tests for ManifestHttpClient

--- test/D2.Manifest.Tests/ManifestUnitTests.cs
+++ test/D2.Manifest.Tests/ManifestUnitTests.cs
@@ -1,10 +1,121 @@
+using System.Net;
+using D2.Manifest.Infrastructure;
+using Moq;
+using Moq.Protected;
+
 namespace D2.Manifest.Tests;
 
 public class ManifestUnitTests
 {
-    [Fact]
-    public void Test1()
+    private readonly Mock<IHttpClientFactory> _mockFactory = new();
+    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler = new();
+    private readonly HttpClient client;
+
+    public ManifestUnitTests()
     {
+        client = new HttpClient(_mockHttpMessageHandler.Object);
+        client.BaseAddress = new Uri("https://www.bungie.net");
+    }
+
+
+    [Fact]
+    public async Task TestGetManifestAsync()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        var manifest = "Test Manifest";
+        _mockHttpMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(manifest)
+            });
+
+        // Act
+        var result = await manifestHttpClient.GetManifestAsync();
+
+        // Assert
+        Assert.Equal(manifest, result);
+    }
     
+    [Fact]
+    public void TestGetManifestAsync_ThrowsHttpRequestException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<HttpRequestException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<HttpRequestException>(() => manifestHttpClient.GetManifestAsync());
+    }
+    
+    [Fact]
+    public void TestGetManifestAsync_ThrowsInvalidOperationException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<InvalidOperationException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<InvalidOperationException>(() => manifestHttpClient.GetManifestAsync());
+    }
+    
+    [Fact]
+    public void TestGetManifestAsync_ThrowsTaskCanceledException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<TaskCanceledException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<TaskCanceledException>(() => manifestHttpClient.GetManifestAsync());
+    }
+    
+    [Fact]
+    public void TestGetManifestAsync_ThrowsUriFormatException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<UriFormatException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<UriFormatException>(() => manifestHttpClient.GetManifestAsync());
+    }
+    
+    [Fact]
+    public void TestGetManifestAsync_ThrowsArgumentNullException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<ArgumentNullException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentNullException>(() => manifestHttpClient.GetManifestAsync());
+    }
+    
+    [Fact]
+    public void TestGetManifestAsync_ThrowsArgumentException()
+    {
+        // Arrange
+        _mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+        var manifestHttpClient = new TestManifestHttpClient(_mockFactory.Object);
+        SetupMock<ArgumentException>();
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentException>(() => manifestHttpClient.GetManifestAsync());
+    }
+
+    private void SetupMock<T>() where T : Exception, new()
+    {
+        _mockHttpMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ThrowsAsync(new T());
     }
 }
---
+++ test/D2.Manifest.Tests/TestManifestHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using D2.Manifest.Application;
+using D2.Manifest.Infrastructure;
+
+namespace D2.Manifest.Tests;
+
+// Derived test TestManifestHttpClient class to override the DownloadAsync method
+
+
+
+
+public class TestManifestHttpClient : ManifestHttpClient
+{
+    public TestManifestHttpClient(IHttpClientFactory factory) : base(factory)
+    {
+    }
+}
