f9e5217306f4a461fdc4e64715402d23e932fc85
Joseph <tanglesites@hotmail.com>
2024-07-31 01:05:11 -0500

    Add ManifestIOClient

---
+++ src/D2.Manifest/Infrastructure/ManifestIOClient.cs
@@ -0,0 +1,16 @@
+﻿using D2.Manifest.Application;
+
+namespace D2.Manifest.Infrastructure;
+
+public class ManifestIOClient : IManifestIOClient
+{
+    private const string ManifestFileName = "manifest.json";
+    public async Task WriteManifestAsync(string manifest)
+    {
+        if (Path.Exists(ManifestFileName))
+        {
+            return;
+        }
+        await File.WriteAllTextAsync(ManifestFileName, manifest);
+    }
+}
