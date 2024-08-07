640a20d69b5c160fca1d6afd4d77d50ff3c5d984
Joseph <tanglesites@hotmail.com>
2024-08-06 21:28:09 -0500

    Add Blazor Client

---
+++ src/D2.Presentation.BlazorClient/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace D2.Presentation.BlazorClient.Models;
+
+public class Server
+{
+    public int ServerId { get; set; }
+    public bool IsOnline { get; set; }
+    public string? Name { get; set; }
+    public string? City { get; set; }
+
+    public Server()
+    {
+        var random = new Random();
+        int randomNumber = random.Next(0, 2);
+        IsOnline = randomNumber == 0 ? false : true;
+    }
+}
